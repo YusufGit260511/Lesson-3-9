cc4127a76fcd3dfd753b9f4aa8ba2eea0f54e001
Yusuf <sultvnhuzaevusufhuza@gmail.com>
2025-05-27 10:55:49 +0500

    Обновил код проекта

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ const App = () => {
   const divRef = useRef(null);
 
   let text = '';
-
+  
   if (soat >=6 && soat <= 18){
     document.body.style.backgroundColor = 'white';
     document.body.style.color = 'black';
-    text = 'Light';
+    text = 'Light'; 
   } else{
     document.body.style.backgroundColor = 'black';
     document.body.style.color = 'white';
@@ -26,12 +26,12 @@ const App = () => {
   document.body.style.fontFamily = 'Itim, cursive';
 
   return (
-    <div ref={divRef} className='w-[766px] h-[557px] shadow-[0_4_4_0_#00000040] shadow-md py-[80px] px-[62px] bg-white rounded-[20px] mt-[80px] flex flex-col justify-between'>
-      <h1 className='text-[32px] font-[400]'>Hello my name is: <span className='text-[#FFEE01]'>{username}</span></h1>
-      <p className='text-[32px] font-[400]'>Today is: <span className='text-[#FFEE01]'>{sana}</span></p>
-      <p className='text-[32px] font-[400]'>Time: <span className='text-[#FFEE01]'>{soat}:{minute}</span></p>
-      <p className='text-[32px] font-[400]'>A random number from 0 to 100: </p>
-      <p className='text-[150px] font-[700] text-center'>{random}</p>
+    <div ref={divRef} className='w-[766px] h-[557px] shadow-md py-[80px] px-[62px] bg-white rounded-[20px] mt-[80px] flex flex-col justify-between text-black'>
+      <h1 className='text-[32px] font-[400] text-black'>Hello my name is: <span className='text-[#FFEE01]'>{username}</span></h1>
+      <p className='text-[32px] font-[400] text-black'>Today is: <span className='text-[#FFEE01]'>{sana}</span></p>
+      <p className='text-[32px] font-[400] text-black'>Time: <span className='text-[#FFEE01]'>{soat}:{minute}</span></p>
+      <p className='text-[32px] font-[400] text-black'>A random number from 0 to 100: </p>
+      <p className='text-[150px] font-[700] text-center text-black'>{random}</p>
     </div>
   )
 }
